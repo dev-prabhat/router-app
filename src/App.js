34cbffecac91048fe9ef726072139c9566975136
Prabhat <singhprabhatp.s.224@gamil.com>
2021-08-28 20:21:29 +0530

    All commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react')
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+  const [loading, setLoading] = useState(false)
+
+  const fetchNews = () => {
+    setLoading(true)
+    fetch(url)
+      .then(result => result.json())
+      // .then(data => console.log(data))
+      .then(data => (setNews(data.hits), setLoading(false)))
+      .catch(error => console.log(error))
+  }
+
+  useEffect(() => {
+    fetchNews()
+  }, [url])
+
+  const handleChange = event => {
+    setSearchQuery(event.target.value)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
+
+  const showLoading = () => {
+    return (loading ? <h2>Loading...</h2> : "")
+  }
+
+  const searchForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+    )
+  }
+
+  const showNews = () => {
+    return (
+      news.map((n, i) => {
+        return (
+          <p key={i}>{n.title}</p>
+        )
+      })
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>News App</h1>
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </div>
-  );
+  )
 }
 
 export default App;
